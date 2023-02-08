66d2945af340ebceb29e3972a4be1780859e682e
Dimitris Traskas <dtraskas@gmail.com>
2023-02-08 16:24:39 +0000

    Fixed backend config

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     key            = "cromon/terraform.tfstate"
-    region         = eu-west-2
+    region         = "eu-west-2"
     dynamodb_table = "tf-state-dynamodb-lock"
     encrypt        = true
   }
