4e0c81c993612af50c82cb022c3e39c90c1bc3ce
Dimitris Traskas <dtraskas@gmail.com>
2023-02-14 15:55:37 +0000

    Using multiple environments on same backend on S3

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -12,7 +12,7 @@ terraform {
 
   backend "s3" {
     bucket         = "${var.account_name}-terraform-remote-state"
-    key            = "cromon/terraform.tfstate"
+    key            = "cromon/environments/${var.env}/terraform.tfstate"
     region         = "eu-west-2"
     dynamodb_table = "tf-state-dynamodb-lock"
     encrypt        = true
