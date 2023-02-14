7975df3b6bdf5af863b31df83dd16aaf705db1d9
Dimitris Traskas <dtraskas@gmail.com>
2023-02-14 17:23:10 +0000

    Split provider and backend

---
+++ terraform/backend.tf
@@ -0,0 +1,16 @@
+terraform {
+  backend "s3" {
+    bucket         = "terraform-remote-state"
+    key            = "terraform.tfstate"
+    region         = "eu-west-2"
+    dynamodb_table = "tf-state-dynamodb-lock"
+    encrypt        = true
+  }
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~>4.0"
+    }
+  }
+}
