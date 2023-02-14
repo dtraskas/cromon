9fca67ece51c2fe6e464557c916264488e61a193
Dimitris Traskas <dtraskas@gmail.com>
2023-02-14 22:35:43 +0000

    Modified app bucket

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,12 +1,15 @@
-resource "aws_s3_bucket_public_access_block" "app_bucket" {
-  bucket                  = "${var.account_id}-${var.env}-app"
-  block_public_acls       = true
-  block_public_policy     = true
-  ignore_public_acls      = true
-  restrict_public_buckets = true
-  force_destroy           = true
+resource "aws_s3_bucket" "app_bucket" {
+  bucket = "${var.account_id}-${var.env}-app"
   tags = {
     Owner       = "cromon"
     Environment = "${var.env}"
   }
+}
+
+resource "aws_s3_bucket_public_access_block" "app_bucket" {
+  bucket                  = aws_s3_bucket.app_bucket.id
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
 }
