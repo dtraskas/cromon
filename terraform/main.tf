7fb9ae0760e05196e3044a1471d2efc988209ab2
Dimitris Traskas <dtraskas@gmail.com>
2023-02-14 22:28:55 +0000

    Added no public S3

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,4 +1,12 @@
-# resource "aws_s3_bucket" "app_bucket" {
-#   bucket        = "${var.account_id}-${var.env}-app"
-#   force_destroy = true
-# }
+resource "aws_s3_bucket_public_access_block" "app_bucket" {
+  bucket        = "${var.account_id}-${var.env}-app"
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
+  force_destroy = true
+  tags = {
+    Owner       = "cromon"
+    Environment = "${var.env}"
+  }
+}
